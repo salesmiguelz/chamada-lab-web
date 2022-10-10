8333fc638079302e12887d1b8a3fb7fa727dd058
salesmiguelz <miguelsales02@gmail.com>
2022-10-10 17:12:10 -0300

    Página home dinâmica com presença dos alunos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Models\Student;
+use App\Models\Roll;
 
 class HomeController extends Controller
 {
@@ -11,7 +12,18 @@ class HomeController extends Controller
         $randomImgs = ['bombril.png', 'nescau.png', 'redbull.jpg', 'adidas.png', 'guarana.jpg', 'steam.png'];
 
         $selectedImg = $randomImgs[array_rand($randomImgs)];
-        $users = User::all();
-        return view('home.home', compact('users', 'selectedImg'));
+        $students = Student::all();
+
+        foreach ($students as $student) {
+            $student->presences = Roll::where("student_id", $student->id)->where("attendance", 1)->count();
+            $student->absences = Roll::where("student_id", $student->id)->where("attendance", 0)->count();
+
+            if (($student->presences + $student->absences) == 0) {
+                $student->frequency = 0;
+            } else {
+                $student->frequency = $student->presences / ($student->presences + $student->absences) * 100;
+            }
+        }
+        return view('home.home', compact('students', 'selectedImg'));
     }
 }
--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -15,17 +15,19 @@
                 <th scope="col">Nome</th>
                 <th scope="col">Presenças</th>
                 <th scope="col">Faltas</th>
+                <th scope="col">Frequência</th>
                 <th scope="col">Situação</th>
             </tr>
         </thead>
         <tbody>
-            @foreach($users as $user)
+            @foreach($students as $student)
             <tr>
-                <th scope="row">{{$user->id}}</th>
-                <td>{{$user->name}}</td>
-                <td>0</td>
-                <td>0</td>
-                <td></td>
+                <th scope="row">{{$student->id}}</th>
+                <td>{{$student->name}}</td>
+                <td>{{$student->presences}}</td>
+                <td>{{$student->absences}}</td>
+                <td>{{$student->frequency}}%</td>
+                <td>{{$student->frequency > 75 ? 'Aprovado' : 'Reprovado por RI'}}</td>
             </tr>
             @endforeach
         </tbody>
